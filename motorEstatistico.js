504b0e7207fcf86e3361df46088346750cab5730
inteligenceatlas-web <inteligenceatlas@gmail.com>
2026-09-01 22:45:58 -0400

    Update motorEstatistico.js

--- motorEstatistico.js
+++ motorEstatistico.js
@@ -1,5 +1,7 @@
 // Removemos o import antigo e usamos o math global injetado pelo index.html
-const math = typeof window !== 'undefined' ? window.math : globalThis.math;
+import { create, all } from 'https://jsdelivr.net';
+const math = create(all);
+
 
 function cdfNormal(x) {
     const b1 = 0.319381530, b2 = -0.356563782, b3 = 1.781477937, b4 = -1.821255978, b5 = 1.330274429;
